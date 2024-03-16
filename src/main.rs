0151100d2f09959826ea624338a041ab9077a7a0
Angelo Carly <angelo.carly@protonmail.com>
2024-03-16 22:04:03 +0100

    Hexagon grid

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,32 @@ fn setup(
 ) {
     commands.spawn(Camera2dBundle::default());
 
-    let shape = Mesh2dHandle(meshes.add(RegularPolygon::new(50.0, 6)));
-
     // Distribute colors evenly across the rainbow.
-    let color = Color::hsl(190., 0.95, 0.7);
 
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: shape,
-        material: materials.add(color),
-        transform: Transform::from_xyz(
-            0.0,
-            0.0,
-            0.0,
-        ),
-        ..default()
-    });
+    let size = 50.0;
+    let width = 3.0_f32.sqrt() * size;
+    let height = 3.0 / 2.0 * size;
+
+    for w in 0..6 {
+        for h in 0..6 {
+            let x = width * (w as f32) + (h % 2) as f32 * width / 2.0;
+            let y = height * (h as f32);
+
+            let shape = Mesh2dHandle(meshes.add(RegularPolygon::new(size, 6)));
+            let color = Color::hsl(44. * w as f32 + 9.0 * h as f32, 0.95, 0.7);
+
+            commands.spawn(MaterialMesh2dBundle {
+                mesh: shape,
+                material: materials.add(color),
+                transform: Transform::from_xyz(
+                    x,
+                    y,
+                    0.0,
+                ),
+                ..default()
+            });
+        }
+    }
 }
 
 fn main() {
